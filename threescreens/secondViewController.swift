602ff771fb25f6905168e2278081dbba1b46030d
Hogoworld <hogoworld@hogoworlds-mac-mini.local>
2019-02-06 14:50:55 +0530

    1.added second view in storyboard assign to second view controller to it 2.changes color to a button to it.

---
+++ threescreens/secondViewController.swift
@@ -0,0 +1,30 @@
+//
+//  secondViewController.swift
+//  threescreens
+//
+//  Created by Hogoworld on 06/02/19.
+//  Copyright © 2019 Hogoworld. All rights reserved.
+//
+
+import UIKit
+
+class secondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
